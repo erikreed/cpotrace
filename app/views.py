04a28999c01abbfb8518bbc33217bd8d58cbf9a0
Erik Reed <ereed.uw@gmail.com>
2016-11-07 17:33:35 -0800

    add react v2

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from app.models import Car
 from django.http import JsonResponse
+from django.utils import timezone
 
 
 def cars(request):
-    return JsonResponse(list(Car.objects.all().values(
+    cars = Car.objects.filter(
+        country_code='US',
+        last_seen__gte=timezone.now() - timezone.timedelta(days=1),
+    )
+    return JsonResponse(list(cars.values(
         'first_seen', 'last_seen', 'price', 'is_autopilot',
         'badge', 'metro_id', 'country_code',
-        'odometer', 'year', 'model', 'paint', 'title_status'
+        'odometer', 'year', 'model', 'paint', 'title_status', 'vin'
     )), safe=False)
--- cpotrace/settings.py
+++ cpotrace/settings.py
@@ -28,12 +28,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3004',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
